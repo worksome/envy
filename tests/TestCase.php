8974bebfd3cf9771bad21cbb350702b80bff0eab
Luke Downing <lukeraymonddowning@gmail.com>
2022-01-31 10:30:13 +0000

    Initial commit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,7 +13,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Worksome\\Envsync\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'Worksome\\Envsync\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
         );
     }
 
