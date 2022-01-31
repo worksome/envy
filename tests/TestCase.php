ba3ea95a07bd48f891198dbe91b7f997999a9ed8
Luke Downing <lukeraymonddowning@gmail.com>
2022-01-31 12:34:15 +0000

    Initial commit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,14 +8,35 @@ use PhpParser\ParserFactory;
 use Worksome\Envsync\Contracts\Finder;
 use Worksome\Envsync\EnvsyncServiceProvider;
 use Worksome\Envsync\Tests\Doubles\TestFinder;
+use function Safe\file_get_contents;
+use function Safe\file_put_contents;
 
 class TestCase extends Orchestra
 {
+    private static array $filesToReset = [
+        __DIR__ . '/Application/.env.example',
+    ];
+
+    private array $fileContents = [];
+
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->app->bind(Finder::class, TestFinder::class);
+
+        foreach (self::$filesToReset as $path) {
+            $this->fileContents[$path] = file_get_contents($path);
+        }
+    }
+
+    protected function tearDown(): void
+    {
+        foreach (self::$filesToReset as $path) {
+            file_put_contents($path, $this->fileContents[$path]);
+        }
+
+        parent::tearDown();
     }
 
     protected function getPackageProviders($app)
